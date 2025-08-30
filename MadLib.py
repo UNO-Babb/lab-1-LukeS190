2a1ebb111565883f5df38670b2c9e8aad2900dc0
LukeS190 <48619535@nebraska.edu>
2025-08-30 02:23:37 +0000

    Done with lab

--- MadLib.py
+++ MadLib.py
@@ -1,14 +1,25 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name:Luke Sheardown
+#Date:8-29-2025
+#Assignment:Lab 1
 
 def main():
   print("Madlib")
   #Ask user for words
+noun1 = input("enter a noun: ")
+noun2 = input("enter a noun: ")
+adjective1 = input("enter an adjective: ")
+verb1 = input("enter a verb: ")
+verb2 = input("enter a verb: ")
+adjective2 = input("enter an adjective: ")
 
   #Print the story with the user supplied words.
-
+print("I love my dog " + noun1 + ".")
+print("She loves to go on rides in the " + noun2 + ".")
+print("She is just so " + adjective1 + "!")
+print("I take her on long " + verb1 + ".")
+print("When I first got her I didn't know she could " + verb2 + "!")
+print("Sometimes when she plays outside she can get a little " + adjective2 + ".")
 
 
 #Call the main function if this is the file being run.
